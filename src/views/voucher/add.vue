c1e3b10f0a35a09d95f41c64a2b68cb69a3be859
MFadlyA <muhammadfadly1802@gmail.com>
2023-12-02 15:17:25 +0700

    UTS WEB 23 Kelompok Intelligent Geek

---
+++ src/views/voucher/add.vue
@@ -0,0 +1,147 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="mr40 mt20">
+                    <router-link to="/list-voucher">
+                        <img src="../../assets/icons/back.svg">
+                    </router-link>
+                </div>
+                <div>
+                    <h1 class="m0">Add Voucher</h1>
+                    <p class="mt5 mb20 tc-green">Make your customers shop using vouchers</p>
+                </div>
+            </div>
+            <hr class="uline-grey">
+            <div class="mt50 row">
+                <div class="w20">
+                    <label class="form-label">Voucher Image</label>
+                </div>
+                <div class="w80">
+                    <input
+                        class="form-text"
+                        type="image"
+                        placeholder="Masukkan Kode Voucher"
+                    >
+                </div>
+            </div>
+            <div class="mt50 row">
+                <div class="w20">
+                    <label class="form-label">Voucher Code</label>
+                </div>
+                <div class="w80">
+                    <input
+                        class="form-text"
+                        type="text"
+                        placeholder="Masukkan Kode Voucher"
+                        maxlength="6"
+
+                    >
+                </div>
+            </div>
+            <div class="mt30 row">
+                <div class="w20">
+                    <label class="form-label">Voucher Name</label>
+                </div>
+                <div class="w80">
+                    <input
+                        class="form-text"
+                        type="text"
+                        placeholder="Masukkan Voucher Name"
+                        minlength="5"
+                        maxlength="20"
+                    >
+                </div>
+            </div>
+            <div class="mt30 row">
+                <div class="w20">
+                    <label class="form-label">Description</label>
+                </div>
+                <div class="w80">
+                    <input
+                        class="form-text"
+                        type="text"
+                        placeholder="Masukkan Voucher Description"
+                        maxlength="5000"
+                    >
+                </div>
+            </div>
+            <div class="mt30 row">
+                <div class="w20">
+                    <label class="form-label">Type</label>
+                </div>
+                <div class="w80">
+                    <select class="form-select">
+                        <option disabled selected>Pilih Type</option>
+                        <option value="1">Total</option>
+                        <option value="2">Grand Total</option>
+                        <option value="3">Ongkos</option>
+                    </select>
+                </div>
+            </div>
+            <div class="mt30 row">
+                <div class="w20">
+                    <label class="form-label">Min. Transaction</label>
+                </div>
+                <div class="w80">
+                    <input
+                        class="form-text"
+                        type="text"
+                        placeholder="Masukkan Minimal Transaction"
+                    >
+                </div>
+            </div>
+            <div class="mt30 row">
+                <div class="w20">
+                    <label class="form-label">Amount</label>
+                </div>
+                <div class="w80">
+                    <input
+                        class="form-text"
+                        type="text"
+                        placeholder="Masukkan Amount"
+                    >
+                </div>
+            </div>
+            <div class="mt30 row">
+                <div class="w20">
+                    <label class="form-label">Valid From</label>
+                </div>
+                <div class="w80">
+                    <input
+                        class="form-text"
+                        type="text"
+                        placeholder="Masukkan Tanggal"
+                    >
+                </div>
+            </div>
+            <div class="mt30 row">
+                <div class="w20">
+                    <label class="form-label">Valid Untill</label>
+                </div>
+                <div class="w80">
+                    <input
+                        class="form-text"
+                        type="text"
+                        placeholder="Masukkan Tanggal"
+                    >
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+	name: 'add-voucher',
+    methods: {
+    hitungKarakter(inputElement) {
+      var karakterMasukan = inputElement.value.length;
+      document.getElementById("count").textContent = karakterMasukan;
+    }
+  }
+   
+}
+
+ 
+</script>
---
+++ src/views/voucher/list.vue
@@ -0,0 +1,117 @@
+<template>
+    <div>
+        <Menu/>
+        <div class="container">
+            <h1 class="m0">Voucher</h1>
+            <div class="row">
+                <div class="col">
+                    <p class="mt5 mb20 tc-green">Manage your voucher promotion</p>
+                </div>
+                <div class="col flex">
+                    <router-link to="/add-voucher">
+                        <button class="bt-green">+ Add</button>
+                    </router-link>
+                </div>
+            </div>
+            <div class="row bg-blue mt10 p10">
+                <div class="col">
+                    <input
+                        class="search-field"
+                        type="text"
+                        id="deskripsi"
+                        placeholder="Tambah deskripsi..."
+                    />
+                </div>
+                <div class="col flex">
+                    <button class="bt-blue">Filter</button>
+                </div>
+            </div>
+            <div class="mt50">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>CODE VOUCHER</th>
+                            <th>VOUCHER NAME</th>
+                            <th>VALID FROM</th>
+                            <th>VALID UNTILL</th>
+                            <th>VOUCHER STATUS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="item of list_voucher" :key="item.id">
+                            <td class="center">{{ item.voucher_code }}</td>
+                            <td class="center">{{ item.voucher_name }}</td>
+                            <td class="center"><label class="label-green">{{ item.valid_from }}</label></td> 
+                            <td class="center"><label class="label-green">{{ item.valid_until }}</label></td> 
+                            <td class="center">
+                                <div v-if="item.status === 1">
+                                    <label class="label-green">Active</label>
+                                </div>
+                                <div v-else>
+                                    <label class="label-red">Inactive</label>
+                                </div>
+                            </td>
+                            <td class="center">
+                                <div class="mt5">
+                                    <router-link to="/edit-voucher">
+                                        <img src="../../assets/icons/edit.svg">
+                                    </router-link>
+                                </div>
+                                <div class="mt5">
+                                    <router-link to="/detail-voucher">
+                                        <img src="../../assets/icons/detail.svg">
+                                    </router-link>
+                                </div>
+                                <div class="mt5">
+                                    <img src="../../assets/icons/delete.svg">
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Menu from "../../components/menu.vue"
+
+export default {
+	name: 'list-voucher',
+	components: {
+		Menu
+	},
+    data(){
+        return {
+            // current_date:
+            list_voucher: [
+                {
+                    id: "1",
+                    voucher_code: "ABC123",
+                    voucher_name: "November Ceria",
+                    valid_from: "20-11-2023",
+                    valid_until: "25-11-2023",
+                    status: 1
+                },
+                {
+                    id: "2",
+                    voucher_code: "ABC123",
+                    voucher_name: "November Ceria",
+                    valid_from: "20-11-2023",
+                    valid_until: "25-11-2023",
+                    status: 1
+                },
+                {
+                    id: "3",
+                    voucher_code: "ABC123",
+                    voucher_name: "November Ceria",
+                    valid_from: "20-11-2023",
+                    valid_until: "25-11-2023",
+                    status: 2
+                },
+            ]
+        }
+    }
+}
+</script>
